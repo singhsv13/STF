0759d424ab008e0cf58dbf0ebbfd40a8e85ad40e
SIMARPRIT VIRDI <singhsv3699@gmail.com>
2021-10-10 12:49:37 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-$("#nav-link").click(function() {
-    $('html,body').animate({
-        scrollTop: $("#about-section").offset().top},
-        'slow');
-});
-$("#home-link").click(function() {
-    $("html, body").animate({ scrollTop: 0 }, "slow");
-    return false;
-  });
